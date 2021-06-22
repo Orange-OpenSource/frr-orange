caf3316dc74f626a4765881c6e9364c4df085065
Olivier Dugeon <olivier.dugeon@orange.com>
2021-06-22 20:09:54 +0200

    lib: Update Link State functions to handle IPv6
    In order to add Link State Traffic Engineering to IS-IS, Link State library
    should have been updated:
    
    - Correct Node and Edge RB Tree comparison functions to support key > 32 bits
    - Add new function to convert IS-IS ISO system ID into Vertex or Edge key that
      take into account Endianness architecture
    - Correct Vertex and Edge creation and search function accordingly
    - Add extra Adjacency entries in Link State Attributes for IPv6 Segment Routing
    - Update send/received and show TED functions accordingly
    
    Signed-off-by: Olivier Dugeon <olivier.dugeon@orange.com>

--- lib/link_state.h
+++ lib/link_state.h
@@ -156,6 +156,8 @@ struct ls_node {
 #define LS_ATTR_USE_BW		0x00400000
 #define LS_ATTR_ADJ_SID		0x01000000
 #define LS_ATTR_BCK_ADJ_SID	0x02000000
+#define LS_ATTR_ADJ_SID6	0x04000000
+#define LS_ATTR_BCK_ADJ_SID6	0x08000000
 #define LS_ATTR_SRLG		0x10000000
 
 /* Link State Attributes */
@@ -190,6 +192,10 @@ struct ls_attributes {
 		float rsv_bw;		/* Reserved Bandwidth */
 		float used_bw;		/* Utilized Bandwidth */
 	} extended;
+#define ADJ_PRI_IPV4	0
+#define ADJ_BCK_IPV4	1
+#define ADJ_PRI_IPV6	2
+#define ADJ_BCK_IPV6	3
 	struct ls_adjacency {		/* (LAN)-Adjacency SID for OSPF */
 		uint32_t sid;		/* SID as MPLS label or index */
 		uint8_t flags;		/* Flags */
@@ -198,7 +204,7 @@ struct ls_attributes {
 			struct in_addr addr;	/* Neighbor @IP for OSPF */
 			uint8_t sysid[ISO_SYS_ID_LEN]; /* or Sys-ID for ISIS */
 		} neighbor;
-	} adj_sid[2];		/* Primary & Backup (LAN)-Adj. SID */
+	} adj_sid[4];		/* IPv4/IPv6 & Primary/Backup (LAN)-Adj. SID */
 	uint32_t *srlgs;	/* List of Shared Risk Link Group */
 	uint8_t srlg_len;	/* number of SRLG in the list */
 };
@@ -402,14 +408,24 @@ struct ls_subnet {
 macro_inline int vertex_cmp(const struct ls_vertex *node1,
 			    const struct ls_vertex *node2)
 {
-	return (node1->key - node2->key);
+	if (node1->key == node2->key)
+		return 0;
+	else if (node1->key < node2->key)
+		return -1;
+	else
+		return 1;
 }
 DECLARE_RBTREE_UNIQ(vertices, struct ls_vertex, entry, vertex_cmp);
 
 macro_inline int edge_cmp(const struct ls_edge *edge1,
 			  const struct ls_edge *edge2)
 {
-	return (edge1->key - edge2->key);
+	if (edge1->key == edge2->key)
+		return 0;
+	else if (edge1->key < edge2->key)
+		return -1;
+	else
+		return 1;
 }
 DECLARE_RBTREE_UNIQ(edges, struct ls_edge, entry, edge_cmp);
 
@@ -493,6 +509,16 @@ extern struct ls_vertex *ls_vertex_update(struct ls_ted *ted,
  */
 extern void ls_vertex_clean(struct ls_ted *ted, struct ls_vertex *vertex,
 			    struct zclient *zclient);
+
+/**
+ * This function convert the ISIS ISO system ID into a 64 bits unsigned integer
+ * following the architecture dependent byte order.
+ *
+ * @param sysid The ISO system ID
+ * @return	Key as 64 bits unsigned integer
+ */
+extern uint64_t sysid_to_key(const uint8_t sysid[ISO_SYS_ID_LEN]);
+
 /**
  * Find Vertex in the Link State DB by its unique key.
  *
